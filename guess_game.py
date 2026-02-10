c7d4645d4857ebe57b64d7bfe59aa39e061eead7
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-02-10 13:08:17 +0300

    add main guess game flow

--- guess_game.py
+++ guess_game.py
@@ -7,3 +7,10 @@ def generate_number():
 print("Guess the Number Game")
 def check_guess(secret, guess):
     return secret == guess
+secret = generate_number()
+guess = int(input("Enter number 1-10: "))
+
+if check_guess(secret, guess):
+    print("You win")
+else:
+    print("You lose")
