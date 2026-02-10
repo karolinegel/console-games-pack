61bf15f53ed5bfd21d2c0dc5e72b2f2643dea4bb
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-02-10 13:08:32 +0300

    refactor guess game into function

--- guess_game.py
+++ guess_game.py
@@ -14,3 +14,10 @@ if check_guess(secret, guess):
     print("You win")
 else:
     print("You lose")
+def play_guess_game():
+    secret = generate_number()
+    guess = int(input("Enter number 1-10: "))
+    print("Win" if check_guess(secret, guess) else "Lose")
+
+if __name__ == "__main__":
+    play_guess_game()
